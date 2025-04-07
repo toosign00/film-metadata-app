6f311eeb4e3ba1b4e4183717073aca42cd08ea01
toosign <kevinsoo1014@gmail.com>
2025-04-08 02:00:51 +0900

    footer에 copyright 문구 추가

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,8 @@ const Footer = () => {
           이 도구는 이미지 파일의 EXIF 메타데이터를 브라우저에서 직접 설정합니다.
           <br />
           <span className="text-xs text-gray-500">파일은 서버로 업로드되지 않으며, 모든 처리는 로컬에서 이루어집니다.</span>
+          <br />
+          <span className="text-xs text-gray-500">&copy; 2025. Hyunsoo Ro. All rights reserved.</span>
         </p>
       </div>
     </footer>
