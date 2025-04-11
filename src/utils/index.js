ae89c2897e2543b08a24f9bc659611caf3e7cf46
toosign <kevinsoo1014@gmail.com>
2025-04-11 14:32:14 +0900

    🛠 Rename : 파일 구조 개선 및 유틸리티 기능 확장, jsZIp 에서 fflate 으로 라이브러리 변경 - 기존 코드 구조를 더 모듈화하여 파일별 기능 분리 구현 - fileUtils.js를 convertUtils.js, downloadUtils.js, sortUtils.js로 분할하여 책임 명확화 - 모든 유틸리티를 index.js에서 통합 관리하도록 구조 개선 - 압축 처리 성능 향상을 위해 jsZip 대신 fflate 라이브러리 도입 - 압축 처리 작업을 별도 워커(zipWorker.js)로 분리하여 메인 스레드 블로킹 방지

---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+/**
+ * 유틸리티 함수 통합 내보내기
+ */
+
+// 정렬 유틸리티
+export { naturalSort } from './sortUtils';
+
+// 변환 유틸리티
+export { dataURItoBlob, processBlobInChunks } from './convertUtils';
+
+// 다운로드 유틸리티
+export { downloadFile, downloadAllAsZip } from './downloadUtils';
