0af6f5ccd61da53812a98af698bf2290b37978d0
toosign <kevinsoo1014@gmail.com>
2025-06-05 10:02:35 +0900

    ♻️ refactor: JavaScript를 TypeScript로 변경 TypeScript 적용

---
+++ src/styles/GlobalStyles.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './datepicker.css';
+
+/**
+ * 전역 스타일 컴포넌트
+ * react-datepicker 다크 테마 스타일 적용
+ */
+const GlobalStyles = () => {
+  return null;
+};
+
+export default GlobalStyles;
